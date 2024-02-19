febfe7ba01c4e2d6d2911ea1d154fdc7994177fc
rmj0025 <127268170+rmj0025@users.noreply.github.com>
2024-02-18 18:25:34 -0600

    Initial Commit

---
+++ assignment6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment6
+//
+//  Created by user247998 on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
